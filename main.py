c3acb035dfb88452a2c058b101e2bb0e5b60a533
iAliF <mr.ali.fotouhi@gmail.com>
2022-09-06 18:24:16 +0430

    Check args & start receiving

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
 import argparse
 
+from serial import Serial, SerialException
+
 parser = argparse.ArgumentParser()
 parser.add_argument('port', type=str, help='Serial port')
 parser.add_argument('baud', type=int, help='Baud rate')
 
+serial: Serial
+
+
+def start():
+    while True:
+        try:
+            print(serial.readline())
+        except KeyboardInterrupt:
+            print("Exiting ...")
+            break
+
 
 def main():
     args = parser.parse_args()
+
+    try:
+        global serial
+        serial = Serial(args.port, args.baud)
+    except SerialException:
+        print("couldn't open this port")
+        return
+
+    start()
 
 
 if __name__ == '__main__':
